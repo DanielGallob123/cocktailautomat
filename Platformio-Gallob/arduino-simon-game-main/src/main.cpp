969b8d3a6aa6e4eb6cbe631cf32efd1229a68349
DanielGallob123 <daniel.gallob@ms.bulme.at>
2025-11-20 17:38:17 +0100

    Update main.cpp

--- Platformio-Gallob/arduino-simon-game-main/src/main.cpp
+++ Platformio-Gallob/arduino-simon-game-main/src/main.cpp
@@ -1,10 +1,35 @@
 #include <Arduino.h>
 
+int counter = 0;
+
 void setup() {
   Serial.begin(9600);
+
+  
 }
 
 void loop() {
-  Serial.println("123");
+  counter++;
+
+  if (counter % 3 == 0)
+  {
+    if (counter % 5 == 0)
+    {
+      Serial.println("FizzBuzz");
+    }
+    else{
+      Serial.println("Fizz");
+    }
+  }
+  else {
+    if (counter % 5 == 0) {
+      Serial.println("Buzz");
+    }
+    else {
+      Serial.println(counter);
+    }
+  }
+
   delay(500);
 }
+
